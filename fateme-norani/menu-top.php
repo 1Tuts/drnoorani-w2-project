2f887f40cc6d0f6cb6edcf50128c4073125047cf
fateme-norani <nfnf60@gmail.com>
2012-05-31 20:42:14 +0430

    menu-top

---
+++ fateme-norani/menu-top.php
@@ -0,0 +1,16 @@
+			<div class="menu right last">
+				<ul>
+					<?php
+						wp_list_pages(array(
+							'title_li' => '',
+							'depth'    => 1
+						));
+					?>
+				</ul>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="logo w7 mra"><h1>drnoorani</h1></div>
+			<div class="clear"></div>
+		</div><!-- topheader !-->
+	</div><!-- container-topheader !-->
