89260796b31a10036c78504f5712ba9a4cdd8269
fateme-norani <nfnf60@gmail.com>
2012-05-31 20:42:51 +0430

    images banner

--- fateme-norani/index.php
+++ fateme-norani/index.php
@@ -1,67 +1,12 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>dr noorani</title>
-	<link rel="stylesheet" type="text/css" 
-	href="<?php bloginfo('template_url') ?>/reset.css" />
-	<link rel="stylesheet" type="text/css"
-	 href="<?php bloginfo('template_url') ?>/1styles.css" />
-	<link rel="stylesheet" type="text/css" 
-	href="<?php bloginfo('template_url') ?>/style.css" />
-</head>
-<body>
-	<div class="container-topheader ">
-		<div class="topheader w24">
-			<div class="menu right last">
-				<ul>
-					<?php
-						wp_list_pages(array(
-							'title_li' => '',
-							'depth'    => 1
-						));
-					?>
-				</ul>
-		<div class="clear"></div>
-			</div>
-			<div class="logo w7 mra"><h1>drnoorani</h1></div>
-			<div class="clear"></div>
-		</div>
-	</div>
-	<div class="container-header"></div>
+<?php 
+	get_header();
+	get_template_part('menu','top');
+	get_template_part('banner','top');
+?>
+			
+	
 	<div class="container-addresspage"></div>
 	<div class="container-content">
-		<div class="content w24">
-			<?php
-				if(have_posts()){
-				while(have_posts()){
-				the_post();
-			?>
-			<article class='post'>
-				<h2><?php the_title(); ?></h2>
-				<p>
-					<?php the_content(); ?>
-				</p>
-			</article>
-			<?php
-					}
-				}else{
-					echo 'Nothing ...';
-				}
-			?>
-		</div>
+		<?php get_template_part('content','page');
 	</div>
-	<div class="container-footer">
-		<div class="toggle">
-			<div class="contenfooter w24">
-				<div class="pont"></div>
-				<div class="plus left">+</div>
-				<div class="clear"></div>
-			</div>
-		</div>
-		<div class="footernav">
-			<div class="con-footernav w24"></div>
-		</div>
-	</div>
-</body>
-</html>
+	<?php get_footer(); ?>
