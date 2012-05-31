58e5ef45d0704e3069191b3af47d6a9f5580909c
fateme-norani <nfnf60@gmail.com>
2012-05-31 06:44:38 +0430

    start

---
+++ fateme-norani/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>dr noorani</title>
+	<link rel="stylesheet" type="text/css" 
+	href="<?php bloginfo('template_url') ?>/reset.css" />
+	<link rel="stylesheet" type="text/css"
+	 href="<?php bloginfo('template_url') ?>/1styles.css" />
+	<link rel="stylesheet" type="text/css" 
+	href="<?php bloginfo('template_url') ?>/style.css" />
+</head>
+<body>
+	<div class="container-topheader"></div>
+	<div class="container-header">lkdji ;sidfh sih</div>
+	<div class="container-addresspage"></div>
+	<div class="container-content"></div>
+</body>
+</html>
