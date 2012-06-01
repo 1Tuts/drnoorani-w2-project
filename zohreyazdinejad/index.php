37aed88b645577f731b55f33a528a6183afcd57c
zohreyazdinejad <zohre.yn@gmail.com>
2012-06-01 23:23:07 +0430

    ezafe kardane menu va zir menu

--- zohreyazdinejad/index.php
+++ zohreyazdinejad/index.php
@@ -6,6 +6,8 @@
 <link rel="stylesheet"  type="text/css" href="<?php bloginfo('template_url') ?>/reset.css"/>
 <link rel="stylesheet"  type="text/css" href="<?php bloginfo('template_url') ?>/1styles.css"/>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/style.css" />
+<script type="text/javascript" src="<?php bloginfo('template_url') ?>/jquery.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url') ?>/menu.js"></script>
 </head>
 	<div class="container-topheader">
     	<div class="topheader w24">
