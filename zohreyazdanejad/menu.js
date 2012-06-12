4946ef7cd8b9050ac7f4de587e2e42255066ffe5
unknown <nt@nt-PC.(none)>
2012-06-12 13:53:09 +0430

    fix file

---
+++ zohreyazdanejad/menu.js
@@ -0,0 +1,93 @@
+// JavaScript Document
+
+/*menu
+------------------------------*/
+
+$(function(){
+	$(".topmenu ul").hover(function(){
+		$("li").hover(function(){
+			$("a").slideDown(1400);
+			});
+	
+	
+		});
+});
+
+/*footer
+------------------------------*/
+
+
+/*banner
+-----------------------------*/
+
+var picWidth = 1349,
+currentSlide = 0,
+lastSlide = 0,
+autoPlayDelay = 5000,
+autoPlayTimer,
+autoPlayFn,
+stopAutoPlay,
+startAutoPlay,
+divScrl, ulBtns, liBtns, picsLen, go2slide, nextSlide, prvSlide;
+
+window.onload=function(){
+	
+	divScrl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
+	
+	ulBtns = document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0);
+	liBtns = ulBtns.getElementsByTagName('li');
+	
+	picsLen = liBtns.length;
+	
+	divScrl.style.width = (picsLen*picWidth+100) +'px';
+	ulBtns.style.width = (35*picsLen+5) +'px'; 
+
+	go2slide = function (n){
+		if(n>=picsLen) n=0;
+		if(n<0) n=picsLen-1;
+		
+		divScrl.style.left = -n*picWidth + 'px';
+
+		lastSlide=currentSlide;
+		currentSlide=n;
+
+		liBtns.item(lastSlide).className=''; 
+		liBtns.item(currentSlide).className='active';
+	};
+	
+	nextSlide = function (){
+		go2slide(currentSlide+1)
+	};
+	prvSlide = function (){
+		go2slide(currentSlide-1)
+	};
+
+	for(var i=0;i<picsLen;i++){
+		(function(j){
+			liBtns.item(j).onclick=function(){
+				resetAutoPlay(); 
+				go2slide(j);
+			}
+		})(i);
+	}
+	
+	stopAutoPlay = function (){
+		if(!autoPlayTimer) return; 
+		clearTimeout(autoPlayTimer);
+		autoPlayTimer=0;
+	};
+	startAutoPlay = function (){
+		autoPlayTimer = setTimeout(autoPlayFn,autoPlayDelay);
+	};
+	resetAutoPlay = function (){
+		stopAutoPlay();
+		startAutoPlay(); 
+	};
+
+	(autoPlayFn = function(){
+		if(autoPlayTimer) nextSlide();
+		 else go2slide(currentSlide);
+		startAutoPlay(); 
+	})();
+
+}
