bdecb643dcc6210cba2f5d02af24afce9bbbda07
marzieh akbarpour <m.akbarpour.al@gmail.com>
2012-06-18 16:05:51 +0430

    set menu va zir menu

--- marziye/topmenu.php
+++ marziye/topmenu.php
@@ -9,6 +9,29 @@
 					'depth'    => 1
 				));
 			?>
+            <li><a href="#">خدمات</a>
+            	<ul>
+					<?php
+                        wp_list_pages(array(
+                            'title_li' => '',
+                            'child_of' => 19,
+                            'depth'    => 1
+                        ));
+                    ?>
+            	</ul>
+             </li>
+             
+              <li><a href="#">نمونه کارها</a>
+            	<ul>
+					<?php
+                        wp_list_pages(array(
+                            'title_li' => '',
+                            'child_of' => 21,
+                            'depth'    => 1
+                        ));
+                    ?>
+            	</ul>
+             </li>
 		</ul>
 		 	<div class="clear"></div>
 			</div>
